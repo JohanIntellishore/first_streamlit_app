0b83a3bdbb9066b35c9634dbb2606826ef4dca76
JohanIntellishore <johan.moegelvang@intellishore.dk>
2022-09-21 10:03:10 +0200

    special item

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,5 @@ streamlit.text("🥣 Omega 3 & Blueberry Oatmeal")
 streamlit.text("🥗 Kale, Spinach & Rocket Smoothie")
 streamlit.text("🐔 Hard-boiled free-range egg")
 streamlit.text("🥑🍞 Avocado toast")
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
