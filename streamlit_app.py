dbba7d8e88a8decd91df3d705327f7794c94374e
JohanIntellishore <johan.moegelvang@intellishore.dk>
2022-09-21 16:57:42 +0200

    snowflake connection frontend

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,7 +50,7 @@ except URLError as e:
 
 
 
-streamlit.header("The fruit load list contains:")
+streamlit.header("View our fruit list - Add your favorites!")
 #Snowflake-related functions:
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
