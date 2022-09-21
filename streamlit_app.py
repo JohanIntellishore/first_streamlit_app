71996b663a394880ac2fdfb8617a07c5385d93b8
JohanIntellishore <johan.moegelvang@intellishore.dk>
2022-09-21 10:00:38 +0200

    texts

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,7 @@ import streamlit
 streamlit.title("My Parents New Healthy Diner")
 
 streamlit.header("Breakfast Menu")
+streamlit.text("🥣 Omega 3 & Blueberry Oatmeal")
+streamlit.text("🥗 Kale, Spinach & Rocket Smoothie")
+streamlit.text("🐔 Hard-boiled free-range egg")
+streamlit.text("🥑🍞 Avocado toast")
